54729cb44fbd56b1ff5edad87e8e201e4ec113da
Pavlenko Eugene <pavlenko.obs@gmail.com>
2023-03-08 10:53:34 +0200

    upd

--- src/Client/Authentication/AuthenticationModule.php
+++ src/Client/Authentication/AuthenticationModule.php
@@ -5,7 +5,6 @@ namespace PE\Component\WAMP\Client\Authentication;
 use PE\Component\WAMP\Client\Authentication\Method\MethodInterface;
 use PE\Component\WAMP\Client\Client;
 use PE\Component\WAMP\Client\ClientModuleInterface;
-use PE\Component\WAMP\Client\Session;
 use PE\Component\WAMP\Client\SessionInterface;
 use PE\Component\WAMP\Message\ChallengeMessage;
 use PE\Component\WAMP\Message\HelloMessage;
@@ -19,17 +18,11 @@ final class AuthenticationModule implements ClientModuleInterface
      */
     private array $methods = [];
 
-    /**
-     * @param MethodInterface $method
-     */
     public function addMethod(MethodInterface $method): void
     {
         $this->methods[] = $method;
     }
 
-    /**
-     * @inheritDoc
-     */
     public function attach(EventsInterface $events): void
     {
         $events->attach(Client::EVENT_MESSAGE_RECEIVED, [$this, 'onMessageReceived']);
@@ -45,10 +38,6 @@ final class AuthenticationModule implements ClientModuleInterface
         $events->detach(Client::EVENT_MESSAGE_SEND, [$this, 'onMessageSend']);
     }
 
-    /**
-     * @param Message $message
-     * @param SessionInterface $session
-     */
     public function onMessageReceived(Message $message, SessionInterface $session): void
     {
         if ($message instanceof ChallengeMessage) {
@@ -56,10 +45,6 @@ final class AuthenticationModule implements ClientModuleInterface
         }
     }
 
-    /**
-     * @param Message $message
-     * @param SessionInterface $session
-     */
     public function onMessageSend(Message $message, SessionInterface $session): void
     {
         if ($message instanceof HelloMessage) {
@@ -75,10 +60,6 @@ final class AuthenticationModule implements ClientModuleInterface
         }
     }
 
-    /**
-     * @param SessionInterface $session
-     * @param ChallengeMessage $message
-     */
     private function processChallengeMessage(SessionInterface $session, ChallengeMessage $message): void
     {
         foreach ($this->methods as $method) {
